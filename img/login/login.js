1fd31cb93e33fc6c5c8afdaad4234bace0daea9e
Noob <140767344+PaulRev@users.noreply.github.com>
2023-12-03 06:42:53 -0500

    Add files via upload

---
+++ img/login/login.js
@@ -0,0 +1,8 @@
+// alert('Bienvenido!!!')
+
+
+const login = document.getElementById('btnlogin');
+login.addEventListener('click',btnlogin);
+function btnlogin(){
+    alert('Email Entered');
+}
---
+++ js.js
@@ -0,0 +1,9 @@
+// logo_ID
+// nav
+
+const logo_ID = document.getElementById('logo_ID');
+const nav = document.getElementById('nav');
+
+logo_ID.addEventListener('click',function(){
+    nav.classList.toggle('navllamadoT');
+})
